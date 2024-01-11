c26adf2ebd1084819d5e4e21762fc3c999de2b04
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-01-11 12:52:26 -0500

    uploading code

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page! THIS STINKS!</h1>'
+
+@app.route('/lickysticky')
+def hello():
+    return '<h1>You are nasty.</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
